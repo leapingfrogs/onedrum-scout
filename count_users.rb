7bf55fafb78efefab75d26147cb9840415f486fc
Rob Lally <rob@robertlally.com>
2011-01-13 15:54:31 +0000

    .

---
+++ count_users.rb
@@ -0,0 +1,31 @@
+class CountOneDrumUsers < Scout::Plugin
+  OPTIONS=<<-EOS
+    prefs_file:
+      name: Target Preferences File
+      notes: The full path to the preferences file is required
+  EOS
+    
+  def build_report
+    report :total_accounts => total_accounts, :active_accounts => active_accounts
+  end
+
+  def total_accounts
+    if `grep com.quolos.basicid.username=true server/configuration/.settings/org.eclipse.core.runtime.preferences.OSGiPreferences.3.prefs|wc -l` =~ /(\d+)/
+      $1.to_i
+    else
+      error "Attempt to retrieve total accounts failed"       
+    end
+  end
+     
+  def active_accounts
+    0
+  end
+
+  def prefs_file_path
+    prefs_file = option('prefs_file')
+    
+    error "The path to the prefs file must be set" if prefs_file.nil?
+    
+    return prefs_file.to_s
+  end
+end
