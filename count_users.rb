1474a22c8415f5a439890f6fde640980074b8600
Rob Lally <rob@robertlally.com>
2011-01-13 18:12:41 +0000

    .

--- count_users.rb
+++ count_users.rb
@@ -4,32 +4,31 @@ class CountOneDrumUsers < Scout::Plugin
       name: Target Preferences File
       default: server/configuration/.settings/org.eclipse.core.runtime.preferences.OSGiPreferences.3.prefs
   EOS
-    
+
   def build_report
-    report :total_accounts => total_accounts, :active_accounts => active_accounts
+    report :total_accounts => total_accounts,
+           :active_accounts => active_accounts
   end
 
   def total_accounts
     if `grep com.quolos.basicid.username=true #{prefs_file_path} | wc -l` =~ /(\d+)/
       $1.to_i
     else
-      error "Attempt to retrieve total accounts failed"       
+      error "Attempt to retrieve total accounts failed"
     end
   end
-     
+
   def active_accounts
-      if `grep com.quolos.basicid.lastLogin=true #{prefs_file_path} | wc -l` =~ /(\d+)/
-        $1.to_i
-      else
-        error "Attempt to retrieve active accounts failed"       
-      end
+    if `grep com.quolos.basicid.lastLogin=true #{prefs_file_path} | wc -l` =~ /(\d+)/
+      $1.to_i
+    else
+      error "Attempt to retrieve active accounts failed"
+    end
   end
 
   def prefs_file_path
-    prefs_file = option('prefs_file')
-    
-    error "The path to the prefs file must be set" if prefs_file.nil?
-    
-    return prefs_file.to_s
+    error "The path to the prefs file must be set" if option('prefs_file').nil?
+
+    option('prefs_file')
   end
 end
---
+++ extract_bulk_stats.rb
@@ -0,0 +1,54 @@
+class ExtractBulkStats < Scout::Plugin
+  OPTIONS=<<-EOS
+    data_metrics_log_file:
+      name: Target Log File
+      default: onedrum_data_metrics.log
+  EOS
+
+  def build_report
+    total_users = nil
+    workspaces = nil
+    chat_messages = nil
+    total_files = nil
+    collaborative_files = nil
+
+    lines = File.readlines(data_metrics_log_path)
+
+    until (lines.empty?)
+      line = lines.pop
+
+      if (line =~ /\]/) then
+        line = line[line.index(']') + 1, line.length]
+        line.strip!
+
+        if (line =~ /^{.*report_type[ ]=[ ]bulk_stats.*}$/) then
+          total_users = $1.to_i if total_users.nil? && line =~ /total_users[ ]=[ ](\d*)/
+          workspaces = $1.to_i if workspaces.nil? && line =~ /workspaces[ ]=[ ](\d*)/
+          chat_messages = $1.to_i if  chat_messages.nil? && line =~ /chat_messages[ ]=[ ](\d*)/
+          total_files = $1.to_i if total_files.nil? && line =~ /total_files[ ]=[ ](\d*)/
+          collaborative_files = $1.to_i if collaborative_files.nil? && line =~ /collaborative_files[ ]=[ ](\d*)/
+        end
+      end
+
+      break if total_users && workspaces && chat_messages && total_files && collaborative_files
+    end
+
+    total_users ||= 0
+    workspaces ||= 0
+    chat_messages ||= 0
+    total_files ||= 0
+    collaborative_files ||= 0
+
+    report :total_users => total_users,
+           :workspaces => workspaces,
+           :chat_messages => chat_messages,
+           :total_files => total_files,
+           :collaborative_files => collaborative_files
+  end
+
+  def data_metrics_log_path
+    error "The path to the prefs file must be set" if option('data_metrics_log_file').nil?
+
+    option('data_metrics_log_file')
+  end
+end
