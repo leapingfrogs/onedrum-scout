4b08a6da0b39d61f238e904e77da3755e6071d54
Ian Davies <idavies@leapingfrogs.com>
2011-05-05 14:09:54 +0100

    Fixing online users count extraction

--- extract_users_online.rb
+++ extract_users_online.rb
@@ -21,7 +21,7 @@ class ExtractUsersOnline < Scout::Plugin
         line = line[line.index(']') + 1, line.length]
         line.strip!
 
-        if (line =~ /^[{].*report_type[ ]=[ ][']users_online.*[}]$/) then
+        if (line =~ /^[{].*report_type[ ]=[ ][']users_online['].*[}]$/) then
           users_online = $1.to_i if users_online.nil? && line =~ /count[ ]=[ ](\d*)/
         end
       end
